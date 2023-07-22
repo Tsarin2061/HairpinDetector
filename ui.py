ec40db9bb4c75ad7b13a801137a39e04f239e67a
Tsarin2061 <tsarinlev@gmail.com>
2023-07-23 02:47:53 +0300

    graphical changes

--- ui.py
+++ ui.py
@@ -7,15 +7,22 @@ import os
 import tempfile
 
 def main():
+    st.set_page_config(
+    page_title="HairpinDetector",
+    page_icon="🧊",
+    initial_sidebar_state="expanded")
 # about app
     st.title('HairpinDetector')
-    st.subheader('Poorly writen app to detect hairpins and analyse them.')
+    st.text('Poorly writen app to detect hairpins and analyse them.')
     # upload file 
     files = st.file_uploader('Upload your fasta file here', accept_multiple_files=True, type = ['fasta','fa'])
     # parameters of hairpin
-    loop_length = st.number_input('Length of a loop',0,10)
-    stem_length = st.number_input('Length of a stem',0,10)
-    threshold = st.number_input('GC count in a stem',0,10)
+    columns = st.columns([1, 1, 1])
+    loop_length = columns[0].number_input('Length of a loop',0,10)
+    stem_length = columns[1].number_input('Length of a stem',0,10)
+    threshold = columns[2].number_input('GC count in a stem',0,10)
+
+
 
     for file in files:
 
@@ -30,9 +37,12 @@ def main():
         for seq in sequences:
             st.write(f"ID: {seq.id}")
         if st.button('Search') and stem_length is not None and loop_length is not None and threshold is not None:
+            # applying function to find hairpins and filter them 
             hairpins_df = parse_seq(seq.seq, stem_length,loop_length)
             filtered_df = filter_df(hairpins_df,threshold)
+            # display df
             st.write(filtered_df)
+
 
             st.download_button(
                 label="Download data as CSV",
